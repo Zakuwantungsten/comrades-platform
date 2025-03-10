d2505b85efc2a3175898b0c215328e1cb4dc24df
Zakuwantungsten <zakuwantungsten@gmail.com>
2025-03-10 17:55:58 +0300

    adding a user model  edited the schema

---
+++ backend/models/User.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }  , 
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    role: {
+        type: String,
+        required: true
+    } ,        
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+}
+);
+// Hash the password before saving the user model
+UserSchema.pre('save', async function(next) {
+    const user = this;
+    if (this.isModified('password') || this.isNew) {
+        try {
+            const hash = await bcrypt.hash(user.password, 10);
+            user.password = hash;
+            next();
+        } catch (error) {
+            next(error);
+        }
+    } else {
+        return next();
+    }
+} );
+// Method to compare password for login
+UserSchema.methods.comparePassword = function(pw) {
+    return bcrypt.compare(pw, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
