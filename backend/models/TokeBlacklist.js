43fe6ff917e18a98fb3518a7b02dff8179bff069
Zakuwantungsten <zakuwantungsten@gmail.com>
2025-03-12 20:08:49 +0300

    authentification

---
+++ backend/models/TokeBlacklist.js
@@ -0,0 +1,30 @@
+import express from "express";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getUser,
+  makeAdmin,
+  deleteUser
+} from "../controllers/authController.js";
+import auth from "../middleware/auth.js";
+import { userValidationRules, loginValidationRules, makeAdminValidationRules } from "../middleware/validators.js";
+import rateLimit from "express-rate-limit";
+
+const router = express.Router();
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // Limit each IP to 5 requests
+  message: "Too many login attempts, please try again later",
+});
+
+// Routes
+router.post("/register", userValidationRules, registerUser);
+router.post("/login", loginValidationRules, authLimiter, loginUser);
+router.get("/me", auth, getUser);
+router.post("/logout", auth, logoutUser);
+router.post("/make-admin", auth(true), makeAdminValidationRules, makeAdmin);
+router.delete("/:id", auth(true), deleteUser);
+
+export default router;
