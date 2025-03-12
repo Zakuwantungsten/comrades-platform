248478e2bc11922e8fa2d4ed65f5a3502fdfe078
Zakuwantungsten <zakuwantungsten@gmail.com>
2025-03-12 20:08:06 +0300

    authentification  refactoring

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import connectDB from "./config/mongoDBconnect.js";
+import cors from "cors";
 // Connect to MongoDB
 connectDB();
 const app = express();
@@ -8,6 +9,8 @@ const port = 9090;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.use(express.json());
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
