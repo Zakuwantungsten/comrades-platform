b6cebe7cb114b9df8838ab36e9b964e073174a77
Zakuwantungsten <zakuwantungsten@gmail.com>
2025-03-13 12:35:49 +0300

    refactor User model to use bcryptjs and update export syntax

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,18 +1,18 @@
 import mongoose from 'mongoose';
-const Schema = mongoose.Schema;
-import bcrypt from 'bcrypt';
+import bcrypt from "bcryptjs";
 
+const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
     username: {
         type: String,
         required: true,
         unique: true
-    },
+    }, 
     password: {
         type: String,
         required: true
-    }  , 
+    }, 
     email: {
         type: String,
         required: true,
@@ -21,32 +21,31 @@ const UserSchema = new Schema({
     role: {
         type: String,
         required: true
-    } ,        
-
+    },        
     createdAt: {
         type: Date,
         default: Date.now
     }
-}
-);
+});
+
 // Hash the password before saving the user model
-UserSchema.pre('save', async function(next) {
-    const user = this;
+UserSchema.pre('save', async function (next) {
     if (this.isModified('password') || this.isNew) {
         try {
-            const hash = await bcrypt.hash(user.password, 10);
-            user.password = hash;
+            this.password = await bcrypt.hash(this.password, 10);
             next();
         } catch (error) {
             next(error);
         }
     } else {
-        return next();
+        next();
     }
-} );
+});
+
 // Method to compare password for login
-UserSchema.methods.comparePassword = function(pw) {
+UserSchema.methods.comparePassword = function (pw) {
     return bcrypt.compare(pw, this.password);
 };
 
-module.exports = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema);
+export default User;
