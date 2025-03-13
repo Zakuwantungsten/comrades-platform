db61a6de3d5d9af1159d7f2ccee3df2f6ec50db3
Zakuwantungsten <zakuwantungsten@gmail.com>
2025-03-13 08:58:18 +0300

    merging from users-registration

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import connectDB from "./config/mongoDBconnect.js";
+import cors from "cors";
+import authRouter from "./routes/authRoutes.js";
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+const port = 9090;
+
+app.use(express.json());
+app.use(cors());
+
+// Corrected API route
+app.use('/api/auth', authRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
